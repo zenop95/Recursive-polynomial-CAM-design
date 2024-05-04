3c7c4423f8ebde32c93e834bce02976e9646a8ae
zenop95 <74430386+zenop95@users.noreply.github.com>
2024-05-04 13:29:10 +1200

    Update postProcessSingleImpulseEsaCases.m

--- postProcessSingleImpulseEsaCases.m
+++ postProcessSingleImpulseEsaCases.m
@@ -1,3 +1,21 @@
+beep off
+format longG
+close all
+clear
+addpath(genpath('.\data'))
+addpath(genpath('.\Functions'))
+addpath(genpath('.\CppExec'))
+addpath(genpath('.\Path'));
+addpath(genpath('C:\Program Files\Mosek\10.0\toolbox\r2017a'));
+set(0,'DefaultTextInterpreter','latex');
+set(0,'DefaultAxesFontSize',16);
+set(0,'DefaultAxesFontName','Times');
+set(0,'DefaultUicontrolFontName','Times', 'DefaultUicontrolFontSize', 14);
+set(0,'DefaultUitableFontName','Times', 'DefaultUitableFontSize', 14);
+set(0,'DefaultTextFontName','Times', 'DefaultTextFontSize', 14);
+set(0,'DefaultUipanelFontName','Times', 'DefaultUipanelFontSize', 14);
+set(0, 'DefaultLineLineWidth', 1);
+set(0,'defaultfigurecolor',[1 1 1])
 %% load variables
 close all
 n = 2170;
