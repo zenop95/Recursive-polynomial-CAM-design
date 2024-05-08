04c35a65f208f3a395dd687437a19436e0190bcc
zenop95 <74430386+zenop95@users.noreply.github.com>
2024-05-09 10:27:32 +1200

    Update aidaInit.m

--- Functions/Init/aidaInit.m
+++ Functions/Init/aidaInit.m
@@ -7,7 +7,7 @@ function pp = aidaInit(pp,sc,j)
 if strcmpi(pp.orbit,'geo')
     pp.flag1 = 0; % No atmosphere in GEO
 end
-fid = fopen('AIDA_init.dat', 'w');
+fid = fopen('write_read/AIDA_init.dat', 'w');
     fprintf(fid, '%2i\n',     pp.aida.flag1); % atmosphere flag (1:non-rotating, 2:rotating)
     fprintf(fid, '%2i\n',     pp.aida.flag2); % SRP flag (1:no shadow, 2:Earth cylindrical shadow, 3:Earth biconical shadow, 4:Earth and Moon cylindrical shadow, 5:Earth biconical and Moon cylindrical shadow, 6:Earth and Moon biconical shadow)
     fprintf(fid, '%2i\n',     pp.aida.flag3); % third body flag (1:Moon, 2:Moon and Sun)
